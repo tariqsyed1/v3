9f477649d379a0b3623af0ecd1241a3aea711c46
tariqsyed1 <trexrh@gmail.com>
2023-03-22 18:28:27 -0400

    final updates to construction page. deploy to domain

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './Fonts.css';
 import Logo from './Assets/logo.svg';
 
 function App() {
+
   return (
-    <div className="App">
-      <div>
-        <img src={Logo} alt=''/>
-        <p>under construction</p>
-      </div>
+    <div>
+      <img src={Logo} alt=''/>
+      <p>under construction</p>
     </div>
   );
 }
