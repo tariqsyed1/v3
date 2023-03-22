3e6e6f2a974b5a00d2bffaf322766d0ed20d6ff5
tariqsyed1 <trexrh@gmail.com>
2023-03-22 18:50:14 -0400

    refactor construction to its own componnet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
+import Construction from './Components/Construction';
+
 import './App.css';
-import './Fonts.css';
-import Logo from './Assets/logo.svg';
 
 function App() {
 
   return (
-    <div>
-      <img src={Logo} alt=''/>
-      <p>under construction</p>
-    </div>
-  );
+    <Construction />
+    );
 }
 
 export default App;
