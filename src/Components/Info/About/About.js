8d46766f8785ab8b34f5066b874e3760eb11e509
tariqsyed1 <trexrh@gmail.com>
2024-08-27 20:00:07 -0400

    resizing more divs + fixing spacing issues with contact.js

--- src/Components/Info/About/About.js
+++ src/Components/Info/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
             <div className='section'>I'm a software engineer who loves to make cool stuff, currently working at <div className='bold-text'>Amazon</div> in <div className='bold-text'>Toronto</div>.</div>
             <div className='section'>My tech journey began when my computer couldn’t handle the games I wanted to play, so my dad (who's in IT) showed me how to build and troubleshoot PCs. Fast forward to today, and I'm enamoured with tech. Programming is modern day wizardry; developers can create value for people out of thin air.</div>
             <div className='section'>These days you can find me working on <a link='https://www.google.ca'>Sponsored Display</a>, building features to help advertisers maximize their advertising campaigns. I've had the privilege of working at a few different companies - If you’re curious, you can learn more about it <a>here.</a></div>
-            <div className='section'>When I’m not at my computer, I’m probably chilling with my cat or listening to music. If you'd like to chat, feel free to contact me via email or LinkedIn :)</div>
+            <div className='section'>When I’m not at my computer, I’m probably chilling with my <a>cat</a> or listening to <a>music</a>. If you'd like to chat, feel free to contact me via email or LinkedIn :)</div>
         </div>
     );
 }; 
--- src/Components/Info/Contact/Contact.js
+++ src/Components/Info/Contact/Contact.js
@@ -15,11 +15,12 @@ const resume = 'https:/google.ca';
 
 const Contact = () => {
     return (
-        <div className="Contact">
-            <img src={tariq} alt='' style={{'width': '100%'}}/>
+        <div className="contact">
+            <div className='picture'><img src={tariq} alt='' style={{'width': '85%'}}/></div>
             <div className='header'>Tariq Syed</div>
-            <div className='subheading'>Software Engineer | <a href="hello@tariqsyed.dev">hello@tariqsyed.dev</a></div>
-            <div id="Icons">
+            <div className='subheading'>Software Engineer</div>
+            <div><a href="hello@tariqsyed.dev">hello@tariqsyed.dev</a></div>
+            <div className="links">
                 <a href="https://www.github.com/tariqsyed1/" target="_blank" rel="noreferrer" className="social"><GitHubIcon /></a>
                 <a href="https://www.linkedin.com/in/tariqs1" target="_blank" rel="noreferrer" className="social"><LinkedInIcon /></a>
                 <a href="https://drive.google.com/file/d/1FEpV0M9gKq3ngc0khsWNYkaohejwVCQZ/view?usp=sharing" target="_blank" rel="noreferrer" className="social"><DescriptionIcon /></a>
