7dda6fa76cae774cc1c2a07a4d608c0ffebd7a6d
tariqsyed1 <trexrh@gmail.com>
2024-08-28 13:06:25 -0400

    add constants for email/resume/etc

--- src/Components/Info/Contact/Contact.js
+++ src/Components/Info/Contact/Contact.js
@@ -7,11 +7,10 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import DescriptionIcon from '@mui/icons-material/Description';
 import tariq from '../../../Assets/Image/tariq.png'
 
-const location = 'Toronto, Canada';
 const email = 'tariqsyed.dev@gmail.com';
 const linkedin = 'https://www.linkedin.com/in/tariqs1/';
 const github = 'https://github.com/tariqsyed1';
-const resume = 'https:/google.ca';
+const resume = 'https://drive.google.com/file/d/1JXc-GRUl2I_syoygNSNc8Kx2RO11l1sO/view?usp=sharing';
 
 const Contact = () => {
     return (
@@ -19,11 +18,11 @@ const Contact = () => {
             <div className='picture'><img src={tariq} alt='' style={{'width': '85%'}}/></div>
             <div className='header'>Tariq Syed</div>
             <div className='subheading'>Software Engineer</div>
-            <div><a href="hello@tariqsyed.dev">hello@tariqsyed.dev</a></div>
+            <div><a href=''>{email}</a></div>
             <div className="links">
-                <a href="https://www.github.com/tariqsyed1/" target="_blank" rel="noreferrer" className="social"><GitHubIcon /></a>
-                <a href="https://www.linkedin.com/in/tariqs1" target="_blank" rel="noreferrer" className="social"><LinkedInIcon /></a>
-                <a href="https://drive.google.com/file/d/1FEpV0M9gKq3ngc0khsWNYkaohejwVCQZ/view?usp=sharing" target="_blank" rel="noreferrer" className="social"><DescriptionIcon /></a>
+                <a href={github} target="_blank" rel="noreferrer" className="social"><GitHubIcon /></a>
+                <a href={linkedin} target="_blank" rel="noreferrer" className="social"><LinkedInIcon /></a>
+                <a href={resume} target="_blank" rel="noreferrer" className="social"><DescriptionIcon /></a>
             </div>
 
         </div>
